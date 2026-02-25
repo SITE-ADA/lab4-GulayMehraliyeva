b10b0a09d8fe66b2e38b5c9d048764c548e453e4
Gulay Mehraliyeva <gmehraliyeva19925@ada.edu.az>
2026-02-25 16:27:49 +0400

    Add Model Product

---
+++ src/main/java/az/edu/ada/wm2/lab4/controller/ProductController.java
@@ -0,0 +1,34 @@
+package az.edu.ada.wm2.lab4.controller;
+
+import az.edu.ada.wm2.lab4.model.Product;
+import az.edu.ada.wm2.lab4.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public Product create(@RequestBody Product product) {
+        return productService.createProduct(product);
+    }
+
+    @GetMapping("/{id}")
+    public Product getById(@PathVariable UUID id) {
+        return productService.getProductById(id);
+    }
+
+    @GetMapping
+    public List<Product> getAll() {
+        return productService.getAllProducts();
+    }
+}
